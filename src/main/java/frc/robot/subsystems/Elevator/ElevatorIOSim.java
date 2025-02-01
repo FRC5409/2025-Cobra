fe065ff66348b557fe8668d8d93c6f588f9732ec
Jason4Jason <159550090+Jason4Jason@users.noreply.github.com>
2025-02-01 14:30:05 -0500

    Clean up code
    - clean up useless codes

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorIOSim.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.Elevator;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -11,7 +10,6 @@ import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
-import frc.robot.util.StructHelper;
 
 public class ElevatorIOSim implements ElevatorIO {
 
@@ -23,9 +21,7 @@ public class ElevatorIOSim implements ElevatorIO {
 
     private Mechanism2d mech;
     private MechanismRoot2d mainMotor;
-    private MechanismRoot2d followerMotor;
     private MechanismLigament2d mainMotorMech;
-    private MechanismLigament2d followerMotorMech;
 
     public ElevatorIOSim() {
         elevatorSim = new ElevatorSim(DCMotor.getFalcon500Foc(2), 9.0 /1.0, 3.446, 0.0199, 0.0, 1.066, true, 0.0);
@@ -33,10 +29,7 @@ public class ElevatorIOSim implements ElevatorIO {
         PID = new PIDController(1.0, 0.0, 0.0);
         mech = new Mechanism2d(0.927, 10);
         mainMotor = mech.getRoot("mainMotor", 0.25, 0);
-        followerMotor = mech.getRoot("followerMotor", 0.75, 0);
-
         mainMotorMech = mainMotor.append(new MechanismLigament2d("elevator", 1.066, 90));
-        followerMotorMech = followerMotor.append(new MechanismLigament2d("elevator", 1.066, 90));
         
         sb_Sim.add("Elevator Mech", mech);
 
@@ -72,7 +65,6 @@ public class ElevatorIOSim implements ElevatorIO {
         elevatorSim.setInputVoltage (volts);
         elevatorSim.update(0.02);
         mainMotorMech.setLength(1.066+(elevatorSim.getPositionMeters()));
-        followerMotorMech.setLength(elevatorSim.getPositionMeters());
 
         inputs.mainMotorConnected = true;
         inputs.mainAppliedVoltage = volts;
@@ -84,7 +76,7 @@ public class ElevatorIOSim implements ElevatorIO {
         inputs.followerAppliedVoltage = volts;
         inputs.followerAppliedCurrent = current;
         inputs.followerMotorTemperature = 0.0;
-        inputs.followerMotorPosition = followerMotorMech.getLength();
+        inputs.followerMotorPosition = elevatorSim.getPositionMeters();
 
     }
 }
