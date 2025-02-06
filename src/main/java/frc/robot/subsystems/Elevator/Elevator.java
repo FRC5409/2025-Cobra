a7922141c15efb72777baa9303a9932987005aed
Jason4Jason <159550090+Jason4Jason@users.noreply.github.com>
2025-02-06 16:29:42 -0500

    Fixed the json file
    - fixed the json file configuration for simulation to show the robot
    - published pose3d using struct helper

--- src/main/java/frc/robot/subsystems/Elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator/Elevator.java
@@ -3,8 +3,8 @@
 
 package frc.robot.subsystems.Elevator;
 
-import org.littletonrobotics.junction.Logger;
-
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.kElevator;
+import frc.robot.util.StructHelper;
 
 public class Elevator extends SubsystemBase{
 
@@ -22,6 +23,8 @@ public class Elevator extends SubsystemBase{
 
     // Shuffleboard
     private final ShuffleboardTab sb_tab;
+
+    private Pose3d elevatorPose;
 
     private final Alert leftElevatorAlert = new Alert("The Left Motor is Disconnected " + kElevator.MAIN_MOTOR_ID, AlertType.kError);
     private final Alert rightElevatorAlert = new Alert("The Rigth Motor is Disconnected " + kElevator.FOLLOWER_MOTOR_ID, AlertType.kError);
@@ -33,6 +36,9 @@ public class Elevator extends SubsystemBase{
         // Shuffleboard
         sb_tab = Shuffleboard.getTab("Elevator");
         sb_tab.addDouble("Elevator Position", () -> io.getPosition());
+
+        elevatorPose = new Pose3d();
+        StructHelper.publishStruct("Elevator", Pose3d.struct, ()->this.elevatorPose);        
     }
 
     public Command startManualMove(double voltage) {
@@ -62,5 +68,6 @@ public class Elevator extends SubsystemBase{
         io.updateInputs(inputs);
         leftElevatorAlert.set(!inputs.mainMotorConnected);
         rightElevatorAlert.set(!inputs.followerMotorConnected);
+        elevatorPose = new Pose3d(0,0,inputs.mainMotorPosition, new Rotation3d());
     }
 }
