aed7a040d7b48c27294b7f45832ca8d3b74e8c5d
alexanderszura <1szuraale@hdsb.ca>
2025-04-02 19:18:20 -0400

    Update at 'Wed Apr 02 19:18:19 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -687,7 +687,7 @@ public class RobotContainer {
         // NamedCommands.registerCommand("END_WHEN_COLLECTED", Commands.waitUntil(sys_endEffector::coralDetected).withTimeout(1.75));
         NamedCommands.registerCommand("END_WHEN_COLLECTED", Commands.waitUntil(() ->
             sys_endEffector.getCurrent() >= 30 || sys_endEffector.coralDetected()
-        ));
+        ).withTimeout(2.0));
 
         NamedCommands.registerCommand("DRIVE_FORWARD", Commands.runOnce(() -> sys_drive.driveForward(-0.75), sys_drive));
         NamedCommands.registerCommand("BUMP", Commands.runOnce(() -> sys_drive.driveForward(-2.0), sys_drive));
